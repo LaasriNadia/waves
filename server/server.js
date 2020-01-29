fb85330ac92000a6cb72826218924e8a7ffec05f
Laasri Nadia <laasrinadiaa@gmail.com>
2020-01-29 17:07:56 +0100

    fixes

--- server/server.js
+++ server/server.js
@@ -480,8 +480,8 @@ app.post('/api/site/site_data', auth, admin, (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  app.get('/*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   })
 }
 
